ae63506889ccf7c8bf17d2df485968603e5d61e8
Angiephoenix <angiephoenix93@gmail.com>
2023-09-10 23:06:09 +0100

    final commit

--- app.js
+++ app.js
@@ -33,6 +33,12 @@ app.get('/api', (req, res) => {
   res.status(200).json(response);
 });
 
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send('Something broke!');
+});
+
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
